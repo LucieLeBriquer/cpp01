492572c09aaaebca1fc853fcda3bd13d3cd0970f
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-10-27 01:06:15 +0200

    ex02 ok

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,23 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 01:11:05 by lle-briq          #+#    #+#             */
-/*   Updated: 2021/10/25 01:11:05 by lle-briq         ###   ########.fr       */
+/*   Updated: 2021/10/27 01:03:19 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 int	main(void)
 {
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Address of the string : " << &str << std::endl;
+	std::cout << "    using stringPTR " << stringPTR << std::endl;
+	std::cout << "    using stringREF " << &stringREF << std::endl;
+	std::cout << "Content of the string : " << str << std::endl;
+	std::cout << "    using stringPTR " << *stringPTR << std::endl;
+	std::cout << "    using stringREF " << stringREF << std::endl;
 	return (0);
 }
